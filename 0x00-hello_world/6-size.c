af6af277d6ffaef8518b94a3b3e8b368a6c5cd68
kevkiiru <kevkiiru6@gmail.com>
2022-06-02 13:35:11 -0700

    the 6-size commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	double doubleType;
 	char charType;
 
-	printf("Size of int: %zu bytes/n", sizeof(intType));
-	printf("Size of float: %zu bytes/n", sizeof(floatType));
-	printf("Size of double: %zu bytes/n", sizeof(doubleType));
-	printf("Size of char: %zu bytes/n", sizeof(charType));
+	printf("Size of int: %ld bytes/n", sizeof(intType));
+	printf("Size of float: %ld bytes/n", sizeof(floatType));
+	printf("Size of double: %ld bytes/n", sizeof(doubleType));
+	printf("Size of char: %ld bytes/n", sizeof(charType));
 
 	return (0);
 }
