1df7399d41669c6e46c16c8110aff58a215c4001
kevkiiru <kevkiiru6@gmail.com>
2022-06-02 15:30:41 -0700

    the 101 quote commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+# include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,6 +9,6 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful"\" - Dora Korpar, 2015-10-19");
+	write(1, "and that piece of art is useful"\" - Dora Korpar, 2015-10-19", 11);
 	return (1);
 }
