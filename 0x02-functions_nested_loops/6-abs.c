a694516b089f44ebb9cb9c949b546173c23bf45f
kevkiiru <kevkiiru6@gmail.com>
2022-06-07 23:40:17 +0300

    the abs commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# include "main.h"
+
+/**
+ * abs - entry point
+ * @n: checking character
+ * Return: 0 value of n
+*/
+
+int abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
