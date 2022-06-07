171f05866272e12493094a2a9c5a31b3b26ac55d
kevkiiru <kevkiiru6@gmail.com>
2022-06-07 17:34:16 +0300

    the islower commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * @c:- the character to be checked
+ * Return: Always 0 (success)
+*/
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
