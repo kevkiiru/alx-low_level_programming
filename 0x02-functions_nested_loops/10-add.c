90e069e42b573e3e1709937a71dd6b80c11a901a
kevkiiru <kevkiiru6@gmail.com>
2022-06-08 01:04:42 +0300

    the add commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * add - entry point
+ * @x: first int
+ * @y: second int
+ * Return: Always 0 (success)
+*/
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
