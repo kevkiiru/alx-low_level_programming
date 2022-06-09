ff9d82cb17ff9b6171bdc19d60b471dfe4d3800c
kevkiiru <kevkiiru6@gmail.com>
2022-06-09 23:40:12 +0300

    the mul commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - entry point
+ * @a: first character
+ * @b: second character
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (0);
+}
