14504fded60c0b25ef0d25a1a357f1fdd3b16bb1
kevkiiru <kevkiiru6@gmail.com>
2022-06-10 01:15:11 +0300

    the print diagonal commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * print_diagonal - entry point
+ * @n: character in use
+ * Return: 0 always
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 13)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a;
+
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('\');
+		}
+		_putchar('\n');
+	}
+}
