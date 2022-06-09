e346fc24641fda74fba886dcf7ea776762f8cc81
kevkiiru <kevkiiru6@gmail.com>
2022-06-10 00:08:42 +0300

    the print most numbers commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Return: 0 always
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		if (c != 2 && c != 4)
+		{
+			_putchar(c + '0');
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	_putchar('\n');
+}
