5eebeb71090495125a84f25e0c74408b7894feb7
kevkiiru <kevkiiru6@gmail.com>
2022-06-10 01:02:35 +0300

    the print line commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * print_line - entry point
+ * @n: character in use
+ * Return: 0 always
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
