c9f10e042bf05b4861094ac7a5204af020310d3f
kevkiiru <kevkiiru6@gmail.com>
2022-06-09 23:47:57 +0300

    the print numbers commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ *
+ * Return: 0 always
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+}
