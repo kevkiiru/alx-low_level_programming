5e52abe98be2afaa705aebcb548a758cb21fbe3f
kevkiiru <kevkiiru6@gmail.com>
2022-06-10 11:35:56 +0300

    the print triangle commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * print_triangle - entry point
+ * @size: main integer
+ * Return: 0 always
+ */
+
+void print_triangle(int size)
+{
+	int size, a, b;
+
+	if (size <= 10)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a, b;
+
+		for (a = 0; a <= size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(' ');
+			}
+			for (b = 1; b <= a; b++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
