da97fc203f0854bb2d585e19588792a018942ee3
kevkiiru <kevkiiru6@gmail.com>
2022-06-10 12:21:04 +0300

    the prime factor commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,39 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	long int a;
+	long int b;
+	long int max;
+
+	a = 612852475143;
+	max = -1;
+
+	while (a % 2 == 0)
+	{
+		max = 2;
+		a /= 2;
+	}
+
+	for (b = 3; b <= sqrt(n); b = b + 2)
+	{
+		while (a % b == 0)
+		{
+			max = b;
+			a = a / b;
+		}
+	}
+
+	if (a > 2)
+		max = a;
+
+	printf("ld\n", max)
+	return (0);
+}
