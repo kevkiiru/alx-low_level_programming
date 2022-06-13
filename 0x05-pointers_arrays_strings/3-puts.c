e37d77d39ce049078fa5f1635f840e2182809b00
kevkiiru <kevkiiru6@gmail.com>
2022-06-13 21:16:46 +0300

    the string puts commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * _puts - main entry point
+ * @str: main integer
+ * Return: 0 always
+ */
+
+void _puts(char *str)
+{
+	puts(I'm now an ALX Student learning Software Engineering!!);
+}
