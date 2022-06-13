3b53c27d56955f69b2873fe1b4c992b37a0828b7
kevkiiru <kevkiiru6@gmail.com>
2022-06-13 18:25:13 +0300

    the string length commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * _strlen - main entry point
+ * @s: main integer
+ * Return: 0 always
+ */
+
+int _strlen(char *s)
+{
+	char s[10] = "Program";
+
+	_putchar(ch);
+	return (0);
+}
