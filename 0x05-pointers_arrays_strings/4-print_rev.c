8ef936a4a3a5855fc04a37dc291e3674602df10e
kevkiiru <kevkiiru6@gmail.com>
2022-06-13 21:46:53 +0300

    the print reverse commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * print_rev - main entry point
+ * @s: main integer
+ * Return: 0 always
+ */
+
+void print_rev(char *s)
+{
+	int a;
+
+	for (a = 0; a[s] != '\0'; a--)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
