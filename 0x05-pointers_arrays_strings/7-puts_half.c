332a8c16e1a2064eef2f2ce0a7f9b6e6ab921ff6
kevkiiru <kevkiiru6@gmail.com>
2022-06-13 23:08:52 +0300

    the puts half commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * puts_half - main entry point
+ * @str: main integer
+ * Return: 0 always
+ */
+
+void puts_half(char *str)
+{
+	int leng, a, b;
+
+	leng = 0;
+	while (str[len] != '\0')
+	{
+		leng++;
+	}
+
+	if (leng % 2 == 0)
+	{
+		for (b = leng / 2; str[b] != '\0'; b++)
+		{
+			_putchar(str[b]);
+		}
+	}
+	else if (leng % 2)
+	{
+		for (a = (leng - 1) / 2; a < leng - 1; a++)
+		{
+			_putchar(str[a + 1]);
+		}
+	}
+	_putchar('\n');
+}
