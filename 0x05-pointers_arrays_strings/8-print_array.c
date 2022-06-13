03aabc14e05ea764c50db604434da0c084dbac11
kevkiiru <kevkiiru6@gmail.com>
2022-06-13 23:17:29 +0300

    the print array commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * print_array - main entry point
+ * @a: integer one
+ * @n: integer two
+ * Return: 0 always
+ */
+
+void print_array(int *a, int n)
+{
+	int k;
+
+	for (k = 0; k < n; k++)
+	{
+		if (k == 0)
+			printf("%d", a[k]);
+		else
+			printf(", %d", a[k]);
+	}
+	printf('\n');
+}
