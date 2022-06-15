3d1ae1b066f4d14194641a7f5c67883cd55490b9
kevkiiru <kevkiiru6@gmail.com>
2022-06-16 00:10:06 +0300

    the lower to uppercase converter commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+# include "main.h"
+
+/**
+ * string_toupper - main entry point
+ * @s: integer one
+ * Return: 0 always
+ */
+
+char *string_toupper(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] >= 'a' && s[a] <= 'z')
+			s[a] = s[b] - 32;
+	}
+	return (s);
+}
