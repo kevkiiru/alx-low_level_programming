17dac7fbba322d31d7059db78968eb5a621b3d4b
kevkiiru <kevkiiru6@gmail.com>
2022-06-20 16:40:01 +0300

    the memcpy commit file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memcpy - main entry point
+ * @dest: destination of the memory bytes
+ * @src: source of the memory bytes
+ * @n: another character
+ * Return: 0 always
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
