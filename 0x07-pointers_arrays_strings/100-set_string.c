5a4e84c5c8740ce763816ebf5096ed6ae5dbe94e
kevkiiru <kevkiiru6@gmail.com>
2022-06-22 00:52:27 +0300

    the set string commit file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - function that sets the value of a pointer to a char
+ * @s: main character
+ * @to: second character
+ * Return: 0 always
+ */
+
+void set_string(char **s, char *to)
+{
+	char a;
+	int *k;
+
+	a = &k;
+	*k = 10;
+
+	_putchar("Value of char is: ", &k);
+}
