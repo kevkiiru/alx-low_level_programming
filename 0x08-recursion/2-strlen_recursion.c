00b257969ad3cb6eb1948264df8630a7cc62c0c7
kevkiiru <kevkiiru6@gmail.com>
2022-06-22 23:11:26 +0300

    the string length commit file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: main character
+ *
+ * Return: 0 always
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (_strlen_recursion(*s + 1));
+}
