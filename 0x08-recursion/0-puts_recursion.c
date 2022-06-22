e06ce72fca442eff0e3e3d0d6107aabf61803227
kevkiiru <kevkiiru6@gmail.com>
2022-06-22 21:09:06 +0300

    the puts recursion commit file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: main character used
+ *
+ * Return: 0 always
+ */
+
+void _puts_recursion(char *s)
+{
+	s = "This is the string";
+
+	_putchar(s);
+}
