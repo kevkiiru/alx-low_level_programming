48fca9f2f9da7ba88b834ee47d501a24f7215ae0
kevkiiru <kevkiiru6@gmail.com>
2022-06-23 01:11:25 +0300

    the prime number commit file

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_prime_number - function that returns 1 if the input integer is
+ * a prime number, otherwise return 0;
+ * @n: main integer
+ *
+ * Return: 0 always
+ */
+
+int is_prime_number(int n)
+{
+	if (n / 1 == 0 && n / n == 0)
+	{
+		return (1);
+	}
+	return (0);
+}
