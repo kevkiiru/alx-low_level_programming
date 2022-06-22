3b44c887130accc85dcff3c5c570085689af79d5
kevkiiru <kevkiiru6@gmail.com>
2022-06-23 00:14:37 +0300

    the power recursion commit file

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,14 +12,14 @@
 
 int _pow_recursion(int x, int y)
 {
+	if (y == 0)
+	{
+		return (1);
+	}
+
 	if (y < 0)
 	{
 		return (-1);
 	}
-
-	if (y == 0)
-	{
-		return (_pow_recursion(x, y + 1) / x);
-	}
-	return (_pow_recursion(x, y - 1) * x);
+	return (x* _pow_recursion(x, y - 1));
 }
