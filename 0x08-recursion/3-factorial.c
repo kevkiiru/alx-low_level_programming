64460360ef1e32e9500ec647f019827d7bab9733
kevkiiru <kevkiiru6@gmail.com>
2022-06-22 23:53:24 +0300

    the factorial commit file

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,11 +10,14 @@
 
 int factorial(int n)
 {
-	if (n == 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
 
-	factorial(0) = 1;
-	return (n * (factorial(n - 1) + (n - 2)));
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
 }
