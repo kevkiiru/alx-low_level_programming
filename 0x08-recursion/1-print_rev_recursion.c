540242e4d533a1ef68e41c315cb50abb74b88126
kevkiiru <kevkiiru6@gmail.com>
2022-06-22 22:59:08 +0300

    the reverse string commit file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,18 +10,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	char str[];
-	int a, b, leng;
-
-	b = leng - a;
-
-	*s = str[a];
-	str[a] = str[b];
-	str[b] = *s;
-
-	if (a == leng / 2)
-		return;
-
-	_putchar(*s);
-	_print_rev_recursion(str, a + 1, leng);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
