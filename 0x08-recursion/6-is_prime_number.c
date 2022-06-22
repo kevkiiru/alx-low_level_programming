76d7ec38118c4c1e1178d4960c6ebcd1e9670ba5
kevkiiru <kevkiiru6@gmail.com>
2022-06-23 01:39:31 +0300

    the prime number commit file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
+int real_prime_number(int n, int a);
+
 /**
  * is_prime_number - function that returns 1 if the input integer is
  * a prime number, otherwise return 0;
@@ -11,12 +13,27 @@
 
 int is_prime_number(int n)
 {
-	if (n / 1 == 0 && n % n == 0)
-	{
-		return (1);
-	}
-	else
-	{
+	if (n <= 1)
 		return (0);
-		is_prime_number(n, n - 1);
+
+	return (real_prime_number(n, n - 1));
+}
+
+/**
+ * real_prime_number - finds the real prime number
+ * @a: main integer
+ * @i: second integer
+ *
+ * Return: 1 if prime, 0 if not prime
+ */
+
+int real_prime_number(int n, int a)
+{
+	if (a == 1)
+		return (1);
+
+	if (n % a == 0 && a > 0)
+		return (0);
+
+	return (real_prime_number(n, a - 1));
 }
