34004fb26d3b77a168fa47fc9e32f402b9d45fcf
kevkiiru <kevkiiru6@gmail.com>
2022-06-23 23:15:21 +0300

    the palindrome commit file

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 
+int check_palin(char *s, int a, int leng);
+int str_recursion(char *s);
+
 /**
  * is_palindrome - function that returns 1 if a string is a
  * palindrome and 0 if not
@@ -11,20 +14,40 @@
 
 int is_palindrome(char *s)
 {
-	int a, reverse, remainder, original;
+	if (*s == 0)
+		return (1);
+	return (check_palin(s, 0, str_recursion(s)));
+}
 
-	reverse = 0;
-	original = a;
+/**
+ * str_recursion - returns the length of a string
+ * @s: character in use
+ *
+ * Return: length of string
+ */
 
-	while (a != 0)
-	{
-		remainder = a % 10;
-		reverse = reverse * 10 + remainder;
-		a /= 10;
-	}
+int str_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + str_recursion(s + 1));
+}
 
-	if (original == reverse)
-		_putchar(original);
-	else
-		_putchar(original);
+/**
+ * check_palin - checking the character recursively
+ * @a: integer one
+ * @s: main character
+ * @leng: the length of the string
+ * Return: the length of the string
+ */
+
+int check_palin(char *s, int a, int leng)
+{
+	if (*(s + a) != *(s + leng - 1))
+		return (0);
+
+	if (a >= leng)
+		return (1);
+
+	return (check_palin(s, a + 1, leng - 1));
 }
