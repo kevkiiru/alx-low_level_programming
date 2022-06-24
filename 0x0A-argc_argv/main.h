203ceb64b1d966e438ca81021cc55acce5c2769a
kevkiiru <kevkiiru6@gmail.com>
2022-06-24 16:45:57 +0300

    the main header file commit

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,3 +1,3 @@
 int _putchar(char ch);
 int main(int argc __attribute__((unused)), char **argv);
-int main(int argc, char **argv __attribute__((unused)));
+int main(int argc, char *argv[] __attribute__((unused)));
