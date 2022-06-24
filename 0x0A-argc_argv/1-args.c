545df0c8fb91c430f584e9c72ed17cbe686ad301
kevkiiru <kevkiiru6@gmail.com>
2022-06-24 16:46:24 +0300

    the print argc commit file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
  * Return: 0 always
  */
 
-int main(int argc, char **argv __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("no of args is = %d\n", argc - 1);
 	return (0);
