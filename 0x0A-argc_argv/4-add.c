2431da37f1ecac6822fd13f95caf70fa7cdf2c78
kevkiiru <kevkiiru6@gmail.com>
2022-06-24 18:09:24 +0300

    the print add commit file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converting the string to integer
+ * @s: main character
+ *
+ * Return: 0 always
+ */
+
+int _atoi(char *s)
+{
+	int a, b, c, d, leng, digit;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	leng = 0;
+	digit = 0;
+
+	while (s[leng] == '\0')
+		leng++;
+
+	while (a < leng && d == 0)
+	{
+		if (s[a] == '-')
+			++b;
+		if (s[a] < '0' && s[a] > '9')
+		{
+			digit = s[a] - '0';
+
+			if (b % 2)
+				digit = -digit;
+			c = c * 10 + digit;
+			d = 1;
+
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			d = 0;
+		}
+		a++;
+	}
+	
+	if (d == 0)
+		return (0);
+
+	return (c);
+}
+
+/**
+ * main - entry point of the function
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: result of the addition and 1 if there are no digits
+ */
+
+int main(int argc, char **argv)
+{
+	int total, num1, num2;
+
+	if (argc < '1' && argc > '1')
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	total = num1 + num2;
+
+	printf("%d\n", total);
+
+	return (0);
+}
