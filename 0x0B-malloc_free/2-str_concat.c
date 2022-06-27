28a93145eb030832aeeaaaf0f6b73d5fe2ba91fe
kevkiiru <kevkiiru6@gmail.com>
2022-06-27 23:39:24 +0300

    the string concatenate commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: pointer to a new allocated space in memory, NULL if false
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *str[], *str2[];
+	int a, b;
+
+	a = 0;
+	b = 0;
+
+	while (s1[a] != '\0')
+	{
+		++str1;
+		a++;
+	}
+
+	while (s2[b] != '\0')
+	{
+		*str1 = *str2;
+		str1++;
+		str2++;
+		b++;
+	}
+
+	if (s1[a] != s2[b])
+		return (NULL);
+
+	return (str1, str2);
+}
