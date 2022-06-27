b48325eec068b3ea8bca912966454017da6471de
kevkiiru <kevkiiru6@gmail.com>
2022-06-27 17:04:15 +0300

    the create array commit file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_array - function entry point
+ * @size: size of the array
+ * @c: character that returns the function
+ * Return: a pointer if true and NULL if false
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char arr[];
+	*ptr = size;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+
+	return (ptr);
+}
