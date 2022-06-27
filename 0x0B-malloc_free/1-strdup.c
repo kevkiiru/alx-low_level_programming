10b960b7106177275522e450aa9c31dbf5ab0dcb
kevkiiru <kevkiiru6@gmail.com>
2022-06-27 22:57:17 +0300

    the string duplicate commit file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - function that returns a pointer to a new string
+ * @str: the pointer
+ *
+ * Return: a pointer if true and NULL if insufficient
+ */
+
+char *_strdup(char *str)
+{
+	int str2;
+
+	str = (char *) malloc(sizeof(char));
+	str2 = str;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	return (str2);
+}
