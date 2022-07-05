c0d6d27b5539c65e55bb792f6e8817857876cbb7
kevkiiru <kevkiiru6@gmail.com>
2022-07-05 14:21:46 +0300

    the init_dog commit file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/*
+ * init_dog: function that computes the details of the dog
+ * @d: pointer to the struct dog
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: 0 always
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
