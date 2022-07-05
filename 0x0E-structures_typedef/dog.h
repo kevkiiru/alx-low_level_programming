b1edd2ed63ee8bbe4e31846851af2fa9f3135132
kevkiiru <kevkiiru6@gmail.com>
2022-07-05 13:00:38 +0300

    the dog header file commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,32 @@
 #ifndef __DOG_H__
 #define __DOG_H__
 
-#define struct Dog
-
 /**
- * struct Dog - main structure
+ * struct dog - dog's info
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
  *
- * Return: 0 always
  */
 
-struct Dog(char *name, float age, char *owner)
+struct dog
 {
-	return (0);
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_info - the typedef for the struct dog
+ */
+
+typedef struct dog dog_info;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_info *new_dog(char *name, float age, char *owner);
+void free_dog(dog_info *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
