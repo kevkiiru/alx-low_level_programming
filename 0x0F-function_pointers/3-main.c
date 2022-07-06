4499c66cbacf8496c7015041f24dc12b2a7bd92e
kevkiiru <kevkiiru6@gmail.com>
2022-07-07 00:28:33 +0300

    the main c commit file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+
+/**
+ * main - function that performs simple operations
+ * @argc: the arguments
+ * @argv: the array
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	char a;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = *argv[2];
+
+	if ((a == '/' || a == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
