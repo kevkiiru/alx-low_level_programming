c058c4517f8af7f808f007e5dbc79c5d7c714f4c
kevkiiru <kevkiiru6@gmail.com>
2022-07-06 22:52:46 +0300

    the opcodes commit file

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * main - prints the opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes, a;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (a = 0; a < bytes; a++)
+	{
+		if (a == bytes - 1)
+		{
+			printf("%02hhx\n", arr[a]);
+			break;
+		}
+		printf("%02hhx ", arr[a]);
+	}
+	return (0);
+}
