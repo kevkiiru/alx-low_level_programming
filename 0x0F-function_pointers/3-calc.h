3d256cda2b02440887a798aa7ca4b3711b4b8622
kevkiiru <kevkiiru6@gmail.com>
2022-07-07 00:33:54 +0300

    the calc header file commit

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -11,11 +11,11 @@
  * @f: the function
  */
 
-typedef struct op
+typedef struct op op_t
 {
 	char *op;
 	int (*f)(int a, int b);
-}op_t;
+};
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
