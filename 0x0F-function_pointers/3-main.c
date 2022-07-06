e9abbe5665cac36d7c53f6e82f43641c4da6d7c8
kevkiiru <kevkiiru6@gmail.com>
2022-07-07 01:13:12 +0300

    the main c commit file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
+	int num1, num2, result;
 	char a;
 	int (*func)(int, int);
 
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
 
@@ -34,13 +34,13 @@ int main(int argc, char *argv[])
 
 	a = *argv[2];
 
-	if ((a == '/' || a == '%') && arg2 == 0)
+	if ((a == '/' || a == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = func(arg1, arg2);
+	result = func(arg1, num2);
 
 	printf("%d\n", result);
 
