71dddc04171c113d915063c4834058a16b6ef919
kevkiiru <kevkiiru6@gmail.com>
2022-07-08 01:23:14 +0300

    the print all commit file

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything
+ * @format: list of all types of arguments passed to the function
+ *
+ * Return: the correct output
+ */
+
+void print_all(const char * const format, ...)
+{
+	int a = 0;
+	char *str, *sep = "";
+
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[a])
+		{
+			switch (format[a])
+			{
+				case 'c';
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+
+				case 'i';
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+
+				case 'f';
+					printf("%s%f", sep, va_arg(list, int));
+					break;
+
+				case 's';
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default;
+					a++;
+					continue;
+			}
+			sep = ", ";
+			a++;
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
