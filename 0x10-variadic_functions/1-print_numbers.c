eb82d771f2b0b0549da4745687baafa838ef45da
kevkiiru <kevkiiru6@gmail.com>
2022-07-08 00:35:44 +0300

    the print numbers commit file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: string to be printed between numbers
+ * @n: main integer
+ *
+ * Return: nothing if separator is NULL and else otherwise
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int a;
+	va_list list;
+
+	va_start(list, n);
+
+	for (a = 0; a < n; a++)
+	{
+		if (!separator)
+			printf("%d", va_arg(list, int));
+
+		else if (separator && a == 0)
+			printf("%d", va_arg(list, int));
+
+		else
+			printf("%d", separator, va_arg(list, int));
+	}
+
+	va_end(list);
+
+	printf("\n");
+}
