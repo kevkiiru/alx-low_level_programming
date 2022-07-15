17e4bdec689590afa200a688629154c6cadbba18
kevkiiru <kevkiiru6@gmail.com>
2022-07-15 22:24:38 +0300

    the add node end commit file

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node_end - function that appends new node at the end of
+ * linked list
+ * @head: double pointer
+ * @str: string to add new node
+ * Return: the address of new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node;
+	list_t *ptr = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	node = malloc(sizeof(list_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->str = strdup(str);
+	node->len = len;
+	node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
+
+	while (prt->next)
+		ptr = ptr->next;
+
+	ptr->next = node;
+
+	return (node);
+}
