d0b324ac2064df6b15578b2d51e3f39533b85f92
kevkiiru <kevkiiru6@gmail.com>
2022-07-25 22:48:58 +0300

    the create file commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,12 +24,12 @@ int create_file(const char *filename, char *text_content)
 		a++;
 	}
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	leng = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (file == -1)
+	if (leng == -1)
 		return (-1);
 
-	write(file, text_content, a);
+	write(leng, text_content, a);
 
 	return (1);
 }
