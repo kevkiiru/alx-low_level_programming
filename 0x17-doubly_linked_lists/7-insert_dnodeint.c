34fc467484d403b7afe18b63bb1165f2f6de91ce
kevkiiru <kevkiiru6@gmail.com>
2022-09-01 21:59:59 +0300

    the insert dnodeint file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->prev = temp;
 			temp->next->prev = new;
 			temp->next = new;
-			return (new)
+			return (new);
 		}
 		else
 			temp = temp->next;
