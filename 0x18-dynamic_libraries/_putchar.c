5c88f6419b08a66567e6ec852c02dcdb7b09ce16
kevkiiru <kevkiiru6@gmail.com>
2022-09-05 21:24:05 +0300

    the putchar file

--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -3,11 +3,12 @@
 
 /**
  * main - Entry point
+ * @c: main character
  * Description: prints out _putchar
  * Return: Always 0 (success)
 */
 
-int main(void)
+int main(char c)
 {
 	return (write(1, &c, 1));
 }
