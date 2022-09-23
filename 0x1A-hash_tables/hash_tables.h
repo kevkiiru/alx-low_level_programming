10f97789902974b6ee437861f9079594eba719a7
kevkiiru <kevkiiru6@gmail.com>
2022-09-24 01:38:06 +0300

    main header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -92,7 +92,6 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
-
 int replace_value_s(shash_node_t **ht, const char *key, const char *value);
 int check_key_s(shash_node_t *ht, const char *key);
 shash_node_t *add_node_s(shash_node_t **head,
